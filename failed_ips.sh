b471bba38e0f3662385de98e4e1502fd1e77fbca
Charlie Root <s-schmidbauer>
2019-01-17 12:49:34 +0100

    inital commit

---
+++ failed_ips.sh
@@ -0,0 +1,42 @@
+# Makes a list of IPv4 from logs to be added to pf tables.
+##########################################################
+# Get IPv4 from log ($1), insert it into pf table ($) while excluding IP4 from ($5). 
+# Save temporarily to file ($3).
+# Copy temp file to pf table file ($4). 
+# Commit pf table file to git repo in /etc ($4)
+ 
+# Usage: failed_ips.sh /var/log/authlog mofos pf.mofos /etc/pf.mofos /etc/white 
+# Usage: failed_ips.sh /var/log/relayd webmofos pf.webmofos /etc/pf.webmofos /etc/white
+
+echo "get IPv4s from $1"
+
+if [[ $1 = "/var/log/relayd" ]]
+then
+	echo "relayd log"
+	ips=$(cat $1 \
+	| grep '(403' \
+	| grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' \
+	| grep -v -f /etc/white \
+	| sort \
+	| uniq)
+else
+	echo "authlog"
+	ips=$(cat $1 \
+	| grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' \
+	| grep -v -f /etc/white \
+	| sort \
+	| uniq)
+fi	
+
+
+echo "add IPv4s to $2 pf table"
+for ip in $ips ; do doas pfctl -t $2 -T add $ip > /dev/null 2>&1 ; done
+
+echo "dump $2 pf table to file $3"
+doas pfctl -t $2 -T show > $3
+
+echo "copy temp file $3 to persistent file $4"
+doas cp $3 $4
+
+echo "git commit file"
+cd /etc ; doas git add $4 ; doas git commit -am 'update pf table'
