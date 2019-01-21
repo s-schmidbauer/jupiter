0f01fa3e771b9b94f50870d662af4763363854b2
Charlie Root <s-schmidbauer>
2019-01-21 22:10:00 +0100

    update pf table

--- failed_ips.sh
+++ failed_ips.sh
@@ -4,6 +4,7 @@
 # Save temporarily to file ($3).
 # Copy temp file to pf table file ($4). 
 # Commit pf table file to git repo in /etc ($4)
+# Check if pf config check returns OK, then reload
  
 # Usage: failed_ips.sh /var/log/authlog mofos pf.mofos /etc/pf.mofos /etc/white 
 # Usage: failed_ips.sh /var/log/relayd webmofos pf.webmofos /etc/pf.webmofos /etc/white
@@ -37,6 +38,11 @@ doas pfctl -t $2 -T show > $3
 
 echo "copy temp file $3 to persistent file $4"
 doas cp $3 $4
+
+echo "reload pf"
+if doas pfctl -nf /etc/pf.conf; then
+	doas pfctl -f /etc/pf.conf
+fi
 
 echo "git commit file"
 cd /etc ; doas git add $4 ; doas git commit -am 'update pf table'
