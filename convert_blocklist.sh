2b5dce253edac84bca56149fcab79ff11c587cef
Charlie Root <s-schmidbauer>
2019-06-08 22:30:32 +0200

    update block script

--- convert_blocklist.sh
+++ convert_blocklist.sh
@@ -1,21 +1,23 @@
 #convert notracking hosts and domains list into unbound format. takes plenty of memory. 
 
-#https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
+hostnames=https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
 #example:
 #local-zone: "51yes.com" redirect
 #local-data: "51yes.com A 127.0.0.1"
 
-for m in $(grep 0.0.0.0 $2 | cut -d' ' -f2); do \
+curl $hostnames -s -o hostnames.txt
+for m in $(grep 0.0.0.0 hostnames.txt | cut -d' ' -f2); do \
 echo 'local-zone: "'$m'" redirect'; \
 echo 'local-data: "'$m 'A 127.0.0.1"'; \
 done
 
-#https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
+domains=https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt
 #example:
 #address=/dulderbulder.com/0.0.0.0
 #address=/dulderbulder.com/::
 
-for m in $(grep 0.0.0.0 $2 | cut -d'/' -f2); do \
+curl $domains -s -o domains.txt
+for m in $(grep 0.0.0.0 domains.txt | cut -d'/' -f2); do \
 echo 'local-zone: "'$m'" redirect'; \
 echo 'local-data: "'$m 'A 127.0.0.1"'; \
 done
